1f4728e5cb39e76de82bfd8354f0413f276c6e78
DESKTOP-EBEUD5S\thebo <325972@student.mboutrecht.nl>
2021-02-04 15:43:14 +0100

    De juiste versie

--- characters.php
+++ characters.php
@@ -13,39 +13,415 @@
   <body>
   <?php include("navbar.php") ?>
 
-  <div class="container-">
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <div class="container">
+
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="6"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="7"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="8"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="9"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="10"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="11"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="12"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="13"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="14"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="15"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="16"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="17"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="18"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="19"></li>
+    
+
   </ol>
+  <div class= "container-fluid">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="" alt="First slide">
+      <img src="img/Barbarian.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Barbarian</h3>
+      </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
+      <img src="img/Archer (1).png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Archer</h3>
+      </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
+      <img src="img/Giant.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Giant</h3>
+      </div>
+    </div>
+  <div class="carousel-item">
+      <img src="img/Goblin.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Goblin</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Wallbreaker.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Wallbreaker</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Balloon.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Balloon</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Wizard.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Wizard</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Healer.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Healer</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Dragon.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Dragon</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/PEKKA.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>PEKKA</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/witch1.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Witch</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Miner.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Miner</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Valkyrie.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Valkyrie</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/HogRider.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>HogRider</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Golem.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Golem</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/IceWizard.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Ice Wizard</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/ElectoDragon1.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Electo Dragon</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/BarbarianKing3.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Barbarian King</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Archer Queen.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Archer Queen</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/Bowler.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h3>Bowler</h3>
+      </div>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>
+  </div>
+  <!-- hier eindigd carousel-->
 
+  <!-- hier begint cards-->
 
+  <div class="container-fluid">
+  <div class="card-columns p-">
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">>
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Barbarian</h1>
+      <img src="img/Barbarian.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">This fearless warrior relies on his bulging muscles and striking mustache to wreak havoc in enemy villages. Release a horde of Barbarians and enjoy the mayhem!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+    </div>
 
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Archer</h1>
+      <img src="img/Archer (1).png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">These sharpshooters like to keep their distance on the battlefield and in life. Nothing makes them happier than single-mindedly taking down their target.</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
   </div>
 
-    <h1>Dit wordt de characters pagina</h1>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Archer Queen</h1>
+      <img src="img/Archer queen.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">This graceful huntress is a master of destructive force, though modest in health. She snipes targets in her territory when defending, and can summon stealth and terrifying damage when attacking once her Royal Cloak ability is unlocked!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Goblin</h1>
+      <img src="img/Goblin.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">These pesky little creatures only have eyes for one thing: LOOT! They are faster than a Spring Trap, and their hunger for resources is limitless.</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Balloon</h1>
+      <img src="img/Balloon.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">These promoted skeletons have traded in their joy of destroying walls for a joy of destroying defenses. Deploy them to take out pesky mortars and cannons!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Barbarian King</h1>
+      <img src="img/BarbarianKing3.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">This colossal menace soaks up huge amounts of damage and bashes anything in his path. He guards his territory fiercely when defending, and can launch into a devastating rage when attacking once his Iron Fist ability is unlocked!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Bowler</h1>
+      <img src="img/Bowler.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">This big blue dude digs the simple things in life - Dark Elixir drinks and throwing rocks. His massive boulders bounce off their targets and hit again behind it for a double strike!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Dragon</h1>
+      <img src="img/Dragon.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">The might of the dragons are known throughout the land. This scaly terror of the skies feels no mercy and nothing will escape the fiery splashes of his breath.</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Healer</h1>
+      <img src="img/Healer.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">This majestic creature lives to protect and aid her fellow troops. Any army is improved with her healing support, but make sure to protect her from air defenses!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Electo Dragon</h1>
+      <img src="img/ElectoDragon1.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">Possessing iron-tough scales and a breath of devastating lightning, the Electro Dragon's favorite thing is raining destruction from above. When vanquished, the Electro Dragon even pummels the ground with lightning strikes!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Giant</h1>
+      <img src="img/Giant.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">These big guys may seem calm, but show them a turret or cannon and you'll see their fury unleashed! Slow yet durable, these warriors are best used to soak up hits.</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Golem</h1>
+      <img src="img/Golem.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">The mighty Golem loves to soak up damage! When destroyed, it explodes and splits into Golemites. The resulting Golemites have one-fifth the Golem's strength and hitpoints.</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Hog Rider</h1>
+      <img src="img/HogRider.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">Having tamed the fierce leaping hog, the Hog Rider punishes those who hide behind their puny walls! Fueled by Dark Elixir, these warriors have never known defeat!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Ice Wizard</h1>
+      <img src="img/Icewizard.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">This Legendary wizard slows down enemy defenses with destructive shards of ice!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Miner</h1>
+      <img src="img/Miner.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">These sneaky shovelers burrow underground, pass beneath walls, and pop up right next to their targets. While underground, Miners cannot be damaged and will not trigger traps, but still gain bonuses from Spells.</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">PEKKA</h1>
+      <img src="img/PEKKA.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">Is P.E.K.K.A a Knight? A Samurai? A Robot? Nobody knows! P.E.K.K.A's armor absorbs even the mightiest of blows.</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Valkyrie</h1>
+      <img src="img/Valkyrie.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">A master of the two-handed axe, this glorious warrior runs between nearby buildings and can shred several troops or buildings at once with her whirlwind blow!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Wall Breaker</h1>
+      <img src="img/WallBreaker.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">Nothing warms a Wall Breaker's cold and undead heart like blowing up walls. A squad of them will make way for your ground units, and they will do it with a BANG!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Witch</h1>
+      <img src="img/Witch1.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">The Witch never fights alone, constantly raising dead warriors. Upgraded Witches raise more skeletons at a time.</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+      <h1 class="text-center bg-light ">Wizard</h1>
+      <img src="img/Wizard.png" class="card-img-top" alt="...">
+      <div class="card-body bg-light  ">
+        <h5 class="card-title text-">The Wizard is a terrifying presence on the battlefield. Pair him up with some of his fellows and cast concentrated blasts of destruction on anything, land or sky!</h5>
+        <div class="">
+          <a href="page1.html" class="btn btn-primary">info</a>
+        </div>
+      </div>
+    </div>
+</div>
+
     <?php include("footer.php") ?>
 
 
